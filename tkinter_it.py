6867006b7c98a06099f7ee1ab602f69032fce2ae
ninasachs <nsachs@fwparker.org>
2017-05-24 09:41:09 -0500

    hello

--- tkinter_it.py
+++ tkinter_it.py
@@ -5,41 +5,50 @@ class Animals():
     def __init__(self,master):
 
         #Title
-        self.title_font = font.Font(family="Courier New", size=30, weight = font.NORMAL, underline = 2)
+        self.title_font = font.Font(family="Courier New", size=30, weight = font.BOLD, underline = 2)
         self.title = Label(master, text = "Animal Fight Club!", font = self.title_font, bg = "lightgreen", fg = "darkgreen")
-        self.title.grid(row=1, column=1, columnspan=5)
+        self.title.grid(row=1, column=1, columnspan=10)
 
         #Animal Display
         self.animal1_font = font.Font(family = "Courier New", size = 15, weight = font.NORMAL)
         self.animal1 = Label(master, text = "First Animal", fg = "darkgreen")
-        self.animal1.grid(row = 4, column = 1, columnspan = 2)
+        self.animal1.grid(row = 5, column = 1, columnspan = 2)
 
         self.animal2_font = font.Font(family = "Courier New", size = 15, weight = font.NORMAL)
         self.animal2 = Label(master, text = "Second Animal", fg = "darkgreen")
-        self.animal2.grid(row = 4, column = 3, columnspan = 2)
+        self.animal2.grid(row = 5, column = 3, columnspan = 2)
 
         #Add animations
 
         #Question
         self.guess = Label(master, text = "Who will come out on top?")
-        self.guess.grid(row = 5, column = 1, columnspan = 2)
+        self.guess.grid(row = 6, column = 1, columnspan = 2)
 
         #User predicts a winner
-        '''
-        self.winner1_button = Button(self, text = "animal1_name")
-        self.winner1_button.grid(row = 5, column = 2)
 
-        self.winner2_button = Button(self, text = "animal2_name")
-        self.winner2_button.grid(row = 5, column = 3)
-        '''
+        self.winner1_button = font.Font(family = "Courier New", size = 15, weight = font.NORMAL)
+        self.winner1_button = Button(master, text = "animal1_name")
+        self.winner1_button.grid(row = 6, column = 3)
+        self.winner2_button = font.Font(family = "Courier New", size = 15, weight = font.NORMAL)
+        self.winner2_button = Button(master, text = "animal2_name")
+        self.winner2_button.grid(row = 6, column = 4)
+
         #Fight button
+        self.fight_button = font.Font(family = "Courier New", size = 15, weight = font.BOLD)
+        self.fight_button = Button(master, text = "FIGHT!")
+        self.fight_button.grid(row = 7, column = 3, columnspan = 2)
 
         #List of winner and stats
         self.victor = Label(master, text = "Gorilla Wins!  " + "Gorillas are superior in strength, etc")
-        self.victor.grid(row = 6, column = 1, columnspan = 3)
+        self.victor.grid(row = 8, column = 1, columnspan = 3)
 
+        #Play again button
+        self.restart_button = font.Font(family = "Courier New", size = 15, weight = font.NORMAL)
+        self.restart_button = Button(master, text = "Play Again")
+        self.restart_button.grid(row = 8, column = 4)
 
     #def fight_button_click(self):
+
 
 
 if __name__ == "__main__":

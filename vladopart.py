a7272bb230232c8f84233d0e229d3036130221b1
VladoVladVlaVlV <vvojdanovski@fwparker.org>
2017-05-31 09:21:23 -0500

    nb

---
+++ vladopart.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import time
+
+root=tk.Tk()
+canvas=tk.Canvas(root,width=400,height=400)
+canvas.pack()
+circle=canvas.create_oval(50,50,80,80,outline="white",fill="blue")
+
+def redraw():
+   canvas.after(100,redraw)
+   canvas.move(circle,5,5)
+   canvas.update()
+canvas.after(100,redraw)
+root.mainloop()

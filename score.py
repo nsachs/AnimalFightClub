3b8657fc29dd18dda43a719aa9433757ce418f8f
JJ <jbernhardt@fwparker.org>
2017-06-01 10:35:06 -0500

    fix scoring- it works!

--- score.py
+++ score.py
@@ -1,7 +1,15 @@
 """
 Library for scoring with JJ
+
+The scoring data is saved in a google spreadsheet here:
+https://docs.google.com/spreadsheets/d/1SpQrJLBYssPFdoVdB7jft9e4WzW1g14X2Wj3yLVX_gw/edit
+
+Username: animalfightclubscorekeeper@gmail.com
+Password: dperiodisthebest
+
 """
-url = 'https://script.google.com/macros/s/AKfycbzAHgTwxYYtFoMwzyXovL_IUbt62R4hBeoew1wUY4tU/exec?'
+identifier = 'AKfycbydbjC47B3VQQSz_YyaKYMGKr_J11EgsL5upH3A1oPQvSRKncvP'
+url = 'https://script.google.com/macros/s/' + identifier + '/exec?'
 
 from urllib import request
 
@@ -11,5 +19,11 @@ def get_scores():
         print(response.read())
 
 
+def post_score(score):
+    with request.urlopen(url + "type=post&score=" + str(score)) as response:
+        print(response.read())
+
+
 if __name__ == '__main__':
+    post_score(40)
     get_scores()

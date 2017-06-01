96ce928e415394b13c082a10caa35add776d7f0c
JJ <jbernhardt@fwparker.org>
2017-06-01 10:58:27 -0500

    add gs save

--- score.py
+++ score.py
@@ -15,24 +15,34 @@ from urllib import request
 
 
 def get_scores():
+    """
+    Get the scores and output a list of all scores and names in dictionary form
+    like this [{name:name1, score:score1},{name:name2, score:score2}, ... ]
+    :return: a dictionary of scores which are previously saved
+    """
     with request.urlopen(url + "type=fetch") as response:
+        return eval(response.read())
+
+
+def post_score(score, name):
+    """
+    posts the score and name and returns the google script result.
+    :param score: 
+    :param name: 
+    :return: gs result
+    """
+    with request.urlopen(url + "type=post&score=" + str(score) + "&name=" + name.replace(" ", "_")) as response:
         print(response.read())
 
 
-def post_score(score):
-    with request.urlopen(url + "type=post&score=" + str(score)) as response:
-        print(response.read())
-
-
+# FOR TESTING PURPOSES ONLY
 if __name__ == '__main__':
-    post_score(40)
-    get_scores()
-
+    post_score(40, "test name")
+    print(get_scores())
 
 ##########################################################################################
 # The google script associated with the spreadsheet is pasted below for further reference.
 """
-
 function doGet(e) {
   
   if(typeof e !== 'undefined')
@@ -40,36 +50,21 @@ function doGet(e) {
     if(data.type == 'fetch')
       return ContentService.createTextOutput(dumpData());
     if(data.type == 'post')
-      addRow([data.score]) // [col1, col2, col3, col4 ...]
-      return ContentService.createTextOutput("Score of " + data.score + " added to saved scores.");
+      addRow([data.score, data.name]) // [col1, col2, col3, col4 ...]
+      return ContentService.createTextOutput("Score of " + data.score + " for " + data.name + " added to saved scores.");
     return ContentService.createTextOutput("Not a valid request.");
 }
-
 function addRow(e) {
   var sheet = SpreadsheetApp.getActiveSheet();
   sheet.appendRow(e);
 }
-
 function dumpData() {
   var sheet = SpreadsheetApp.getActiveSheet();
   var data = sheet.getDataRange().getValues();
   var out = []
   for (var i = 1; i < data.length; i++) {
-    out.push(JSON.stringify({score: data[i][0]}));
+    out.push(JSON.stringify({score: data[i][0], name: data[i][1]}));
   }
   return out
 }
-
-"https://script.google.com/macros/s/AKfycbzAHgTwxYYtFoMwzyXovL_IUbt62R4hBeoew1wUY4tU/exec?name=labnol&blog=ctrlq&type=get"
-
-/*
-
-
-
-type = fetch, post
-score = value
-
-
-
-*/
 """
